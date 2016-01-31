5f1e95bc352b3b6c9ed1e03c89634cbccf25e701
Roman <romangof@gmail.com>
2016-01-31 18:25:46 -0430

    Initial commit

---
+++ service.rb
@@ -0,0 +1,21 @@
+  require "rest-client"
+
+class Service
+  
+  def self.call_service(args, params='')
+
+    response = RestClient.get( direction(args), {params:params} )
+    
+    if params == ''
+      JSON.parse( response )
+    else
+      response
+    end
+  end
+
+  private
+  def self.direction(args)
+    'http://memecaptain.com/'+args.join("/")    
+  end
+
+end
