87ddf8794aa13c1707cc4ca27e90af460b6c5e63
Julius Indriago <julius_1010@hotmail.com>
2016-02-07 17:07:38 -0430

    adding heroku

--- MemeCapt/db/schema.rb
+++ MemeCapt/db/schema.rb
@@ -13,46 +13,49 @@
 
 ActiveRecord::Schema.define(version: 20160204144858) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
-    t.string   "catname",    limit: 255, null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "catname",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "images", force: :cascade do |t|
-    t.boolean  "privacity",               null: false
-    t.string   "top_text",    limit: 255
-    t.string   "bot_text",    limit: 255
-    t.string   "img_id",      limit: 255, null: false
-    t.string   "ext_url",     limit: 255
-    t.integer  "category_id", limit: 4
-    t.integer  "user_id",     limit: 4
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.boolean  "privacity",   null: false
+    t.string   "top_text"
+    t.string   "bot_text"
+    t.string   "img_id",      null: false
+    t.string   "ext_url"
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "images", ["category_id"], name: "index_images_on_category_id", using: :btree
   add_index "images", ["user_id"], name: "index_images_on_user_id", using: :btree
 
   create_table "images_tags", id: false, force: :cascade do |t|
-    t.integer "tag_id",   limit: 4, null: false
-    t.integer "image_id", limit: 4, null: false
+    t.integer "tag_id",   null: false
+    t.integer "image_id", null: false
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string   "tagname",    limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "tagname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",     limit: 255, null: false
-    t.string   "password",     limit: 255, null: false
-    t.string   "auth_token",   limit: 255, null: false
-    t.string   "token_expiry", limit: 255, null: false
-    t.string   "email",        limit: 255, null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "username",     null: false
+    t.string   "password",     null: false
+    t.string   "auth_token",   null: false
+    t.string   "token_expiry", null: false
+    t.string   "email",        null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   add_foreign_key "images", "categories"
--- MemeCapt/test/models/image_ag_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ImageAgTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
